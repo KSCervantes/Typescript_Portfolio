6f00d970f24e9ef987eb701ebdd989977a91da15
Kyle Cervantes <kylestonehudas@gmail.com>
2025-04-08 20:48:26 +0800

    Create softSkills.tsx

---
+++ src/data/softSkills.tsx
@@ -0,0 +1,8 @@
+// src/data/softSkills.js
+
+export const softSkills = [
+  { name: "Leadership", percentage: 60, color: "from-blue-400 to-indigo-600" },
+  { name: "Teamwork", percentage: 92, color: "from-cyan-400 to-blue-500" },
+  { name: "Communication", percentage: 50, color: "from-indigo-400 to-purple-600" },
+  { name: "Problem Solving", percentage: 40, color: "from-sky-400 to-blue-600" }
+];
