575be0706a6efb2a1b6ebe19e8ac83fbbcd0a035
Kyle Cervantes <kylestonehudas@gmail.com>
2025-04-08 20:50:05 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import Header from './components/Header'
 import Hero from './components/Hero'
 import About from './components/About'
+import Schools from './components/Schools'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -10,8 +12,10 @@ function App() {
       <Header />
       <Hero />
       <About />
+      <Schools />
       <Projects />
       <Contact />
+      <Footer />
     </div>
   )
 }
